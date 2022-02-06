80cd62c3fb1425448e80498b9f1c658c9bce19e2
JaisinhBhosale9712 <71847281+JaisinhBhosale9712@users.noreply.github.com>
2022-02-06 22:00:13 +0100

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,17 +1,17 @@
 import sys
 def fact(n):
-  if n==0:
-    return 1
-  return n*fact(n-1)
+    if n==0:
+      return 1
+    return n*fact(n-1)
   
 def div(n):
-  res=10/n
-  return res
+    res=10/n
+    return res
   
 def main(n):
-  res=fact(n)
-  print(res)
+    res=fact(n)
+    print(res)
   
 if __name__=="__main__":
-  if len(sys.argv)>1:
-    main(int(sys.argv[1]))
+    if len(sys.argv)>1:
+        main(int(sys.argv[1]))
