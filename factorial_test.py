e7f48117f0c6a657efa647a29c1449b95145e8ea
JaisinhBhosale9712 <71847281+JaisinhBhosale9712@users.noreply.github.com>
2022-02-06 22:00:43 +0100

    Update factorial_test.py

--- factorial_test.py
+++ factorial_test.py
@@ -2,9 +2,9 @@ import unittest
 from factorial import fact
 
 class TestFactorial(unittest.TestCase):
-  def test_fact(self):
-  res=fact(5)
-  self.assertEqual(res,120)
+    def test_fact(self):
+        res=fact(5)
+        self.assertEqual(res,120)
   
 if __name__=="__main__":
-  unittest.main()
+    unittest.main()
