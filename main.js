6fbd9f91bb16e51681bc1eaedebf70757738da9c
JaisinhBhosale9712 <71847281+JaisinhBhosale9712@users.noreply.github.com>
2022-02-07 11:07:33 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function Hello(props) {
+  return <h1>Hello World!</h1>;
+}
+if ("a"=="a"){
+  console.log("hu")
+}
+ReactDOM.render(<Hello />, document.geqtElementById('root'));
+ReactDOM.render(<Hello />, document.geqtElementById('root'));
